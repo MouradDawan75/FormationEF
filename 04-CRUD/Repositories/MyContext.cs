71718fd75a3629d6ebbd76ab032ea8d75ac4dcb2
Mourad <mmahrane@dawan.fr>
2025-03-31 14:16:26 +0200

    CRUD

---
+++ 04-CRUD/Repositories/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using _04_CRUD.Models;
+
+namespace _04_CRUD.Repositories
+{
+    public class MyContext : DbContext
+    {
+        
+        public MyContext()
+            : base("name=MyContext")
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+    /*
+     * Enable-Migrations
+     * 
+     */
+   
+}
